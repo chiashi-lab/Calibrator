fbad9ee6dd663d179b7d5caa6b84530686306148
PlusF <rkaneda896@gmail.com>
2023-09-13 12:49:55 +0900

    upgrade show func

--- calibrator/Calibrator.py
+++ calibrator/Calibrator.py
@@ -277,6 +277,6 @@ class Calibrator:
             else:
                 ax.vlines(fitted_x, ymin, ymax, color='r', linewidth=1)
                 ax.vlines(true_x, ymin, ymax, color='b', linewidth=1)
-            ax.text(true_x, ymax*0.95, str(round(true_x, 2)), color='r', fontsize=15)
+            ax.text(fitted_x, ymax*0.95, str(round(fitted_x, 2)), color='r', fontsize=15)
             ax.text(true_x, ymax, str(round(true_x, 2)), color='b', fontsize=15)
         ax.legend()
